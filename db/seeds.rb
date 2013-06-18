c6fd5488ddd5d89ba0f038fd5f0aae4866bc8bd5
Abby Raskin <abbyraskin@gmail.com>
2013-06-18 17:15:30 -0500

    added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+gif_hashes = [
+{ :url => "http://24.media.tumblr.com/tumblr_m3twn1i0l11rpmph5o1_500.gif", :note => "how I felt the first week of Ruby class" },
+{ :url => "http://25.media.tumblr.com/tumblr_mbq7csf16n1r5yc5to1_500.gif", :note => "not convinced this is a real dog" },
+{ :url => "http://www.yourdogplus.com/wp-content/uploads/2013/03/cute-dog-gif-learing-from-lady.gif", :note => "Lenox and I...if i wore heels more."},
+{ :url => "http://25.media.tumblr.com/tumblr_m9kzjsc8RT1rpzjbco1_400.gif", :note => "i don't even know..." }
+]
+
+Gif.destroy_all
+gif_hashes.each do |g_hash|
+  g = Gif.new
+  g.url = g_hash[:url]
+  g.note = g_hash[:note]
+  g.save
+end
+
+puts "There are now #{Gif.count} rows in the gif table."
