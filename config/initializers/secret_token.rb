77fb0c2ea5ce1649a9fc69e8838b555d311ae48b
Abby Raskin <abbyraskin@gmail.com>
2013-06-18 16:39:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doggifs::Application.config.secret_token = '627198c2f8b7a30eebff4cc4dc270779bae2b5d2489369bdff6e8e8d5f17d60867f2b9a89aabc86fe87009e49d6c6a392be812c61bf5e8ade3a37675781aa433'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Doggifs::Application.config.session_store :cookie_store, key: '_doggifs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Doggifs::Application.config.session_store :active_record_store
